d5e459dc8e7f9b14242e4d644d7835c0a30539a0
Pablo Argueta <pabloargueta27@gmail.com>
2018-07-15 18:47:01 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,6 +6,7 @@ def reformat_languages(languages)
   languages.each {|style, languages|
     languages.each {|language, data|
       new_hash[language] = data
+      new_hash[data][:style] << style
     binding.pry
     }
     
